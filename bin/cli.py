b579e743f46bf30a30d2923b719faf5d7bcf26e1
CDTAdmin <cdtadmin@ndmmac60.ndm.ox.ac.uk>
2022-09-29 14:27:16 +0100

    new change

--- bin/cli.py
+++ bin/cli.py
@@ -1,7 +1,8 @@
 import typer
+from ser.infer import def_infer
 
 main = typer.Typer()
-
+here's a change to create a conflict
 
 @main.command()
 def train():
@@ -10,4 +11,4 @@ def train():
 
 @main.command()
 def infer():
-    pass
+    def_infer()
